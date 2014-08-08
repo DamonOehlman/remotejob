038f1ef442513b074035713b125f87b20d4b832b
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-08 17:14:13 +1000

    Check job download functionality

--- test/submit.js
+++ test/submit.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var test = require('tape');
 var uuid = require('uuid');
+var hash = require('hash-files');
 var lastJobNo;
 var lastJob;
 
@@ -29,6 +30,41 @@ module.exports = function(queue) {
       // acknowledge the job (which removes it from the queue)
       job.acknowledge(t.ifErr);
     });
+  });
+
+  test('can download the job to the local file system', function(t) {
+    t.plan(1);
+
+    if (! lastJob) {
+      return t.fail('no job available');
+    }
+
+    lastJob
+      .createReadStream()
+      .once('end', function() {
+        t.pass('downloaded file');
+      })
+      .pipe(fs.createWriteStream(__dirname + '/tmp.zip'));
+  });
+
+  test('compare the uploaded file with the downloaded file', function(t) {
+    var files = [
+      __dirname + '/assets/image-small.zip',
+      __dirname + '/tmp.zip'
+    ];
+    var hashes;
+
+    t.plan(1);
+    hashes = files.map(function(filename) {
+      return hash.sync({ files: [ filename ], noGlob: true });
+    });
+
+    t.equal(hashes[0], hashes[1], 'files match');
+  });
+
+  test('remove the tmp file', function(t) {
+    t.plan(1);
+    fs.unlink(__dirname + '/tmp.zip', t.ifError);
   });
 
   test('able to remove a stored object', function(t) {
