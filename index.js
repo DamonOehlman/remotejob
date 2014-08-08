e8102c39cfa51b43ac389475b0b038a74460d27a
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-08 10:27:55 +1000

    Successfull talking to AWS

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var AWS = require('aws-sdk');
+var async = require('async');
 var debug = require('debug')('remotejob');
+var pluck = require('whisk/pluck');
+var extend = require('cog/extend');
+var EventEmitter = require('events').EventEmitter;
+
+var DEFAULT_Attributes = {};
 
 /**
   # remotejob
@@ -7,27 +13,79 @@ var debug = require('debug')('remotejob');
 
 **/
 module.exports = function(name, opts) {
-  var queue = {};
+  var queue = new EventEmitter();
+  var queueUrl;
+  var accessKeyId = (opts || {}).key;
   var sqs = new AWS.SQS({
     apiVersion: '2012-11-05',
-    accessKeyId: (opts || {}).key,
+    accessKeyId: accessKeyId,
     secretAccessKey: (opts || {}).secret,
-    region: (opts || {}).region || 'us-east-1',
-    logger: {
-      log: function() {
-        console.log(arguments);
-      }
-    }
+    region: (opts || {}).region || 'us-east-1'
   });
+
+  function createQueue() {
+    var opts = {
+      QueueName: name,
+      Attributes: extend({}, DEFAULT_Attributes, (opts || {}).attributes)
+    };
+
+    debug('attempting queue creation: ', opts);
+    sqs.createQueue(opts, function(err, data) {
+      queueUrl = err || (data && data.QueueUrl);
+      queue.emit('ready', queueUrl);
+    });
+  }
+
+  function getQueueAttributes(url, callback) {
+    var opts = {
+      QueueUrl: url,
+      AttributeNames: ['All']
+    };
+
+    debug('attempting to get queue attributes: ', opts);
+    sqs.getQueueAttributes(opts, callback);
+  }
+
+  function getQueueUrl(callback) {
+    var opts = {
+      QueueName: name
+    };
+
+    debug('attempting to get queue url for: ' + name);
+    sqs.getQueueUrl(opts, function(err, response) {
+      if (err) {
+        return callback(err);
+      }
+
+      queue.emit('ready', queueUrl = response.QueueUrl);
+      callback(null, queueUrl)
+    });
+  }
 
   /**
     #### `status(callback)`
 
   **/
   queue.status = function(callback) {
-    debug('attempting to get queue attributes for: ' + name);
-    sqs.getQueueAttributes({ QueueUrl: name }, callback);
+    if (! queueUrl) {
+      return queue.once('ready', function() {
+        queue.status(callback);
+      });
+    }
+
+    if (queueUrl instanceof Error) {
+      return callback(queueUrl);
+    }
+
+    getQueueAttributes(queueUrl, callback);
   };
+
+  // initialise the queueUrl, creating the queue if required
+  getQueueUrl(function(err) {
+    if (err) {
+      createQueue();
+    }
+  });
 
   return queue;
 };
--- test/connect.js
+++ test/connect.js
@@ -1,12 +1,21 @@
 var test = require('tape');
-var jobs = require('..')('test', {
+var queueName = require('uuid').v4();
+var jobs = require('..')(queueName, {
   key: process.env.REMOTEBUILD_TEST_KEY,
   secret: process.env.REMOTEBUILD_TEST_SECRET
+});
+
+test('wait for ready', function(t) {
+  t.plan(1);
+  jobs.once('ready', function() {
+    t.pass('ready for operation');
+  });
 });
 
 test('able to get the status of the queue', function(t) {
   t.plan(1);
   jobs.status(function(err, status) {
     t.ifError(err);
+    console.log(arguments);
   });
 });
